88b15bba1984f9e87faa7e7e0c05cb71401a87fd
Adam Bard <adam@adambard.com>
2019-01-10 21:33:56 -0800

    Almost works

---
+++ app/app.py
@@ -0,0 +1,67 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+import uvicorn
+
+import logging
+
+from pprint import pprint
+
+
+from heuristics import nokillsnek
+
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+
+app = Starlette()
+
+
+def get_weights(board_state):
+    # TODO more smartness
+    return nokillsnek.apply(board_state)
+
+
+def get_move(weights):
+    u, r, d, l = weights
+
+    if u >= r and u >= d and u >= l:
+        return "up"
+    elif r >= d and r >= l:
+        return "right"
+    elif d >= l:
+        return "down"
+    else:
+        return "left"
+
+
+@app.route('/start', methods=['POST'])
+async def start(request):
+    return JSONResponse({'color': 'purple'})
+
+
+@app.route('/move', methods=['POST'])
+async def move(request):
+    pprint(await request.json())
+
+    weights = get_weights(await request.json())
+
+    logger.debug(weights)
+
+    return JSONResponse({'move': get_move(weights)})
+
+
+@app.route('/end', methods=['POST'])
+async def end(request):
+    return JSONResponse({})
+
+
+@app.route('/ping', methods=['POST'])
+async def ping(request):
+    return JSONResponse({})
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
---
+++ app/heuristics/nokillsnek.py
@@ -0,0 +1,83 @@
+"""
+board_state:
+
+    {
+        "board": {
+            "height",
+            "width",
+            "food": [
+                {"x": int, "y": int}
+            ],
+            "snakes": [
+                {"id",
+                 "name",
+                 "health",
+                 body: [
+                   {"x": int, "y": int},
+                   ...
+                 ]
+                },
+            ],
+            "you": {
+                {
+                    "id",
+                    "name",
+                    "health",
+                    "body": [
+                        {"x": int, "y": int},
+                        ...
+                    ]
+                }
+            }
+        }
+    }
+
+"""
+
+def move(pos, d):
+    if d == "u":
+        return {"x": pos["x"], "y": pos["x"] - 1}
+    elif d == "r":
+        return {"x": pos["x"] + 1, "y": pos["y"]}
+    elif d == "d":
+        return {"x": pos["x"], "y": pos["x"] + 1}
+    elif d == "l":
+        return {"x": pos["x"] - 1, "y": pos["y"]}
+    else:
+        return pos
+
+def surroundings(pos):
+    return [
+        move(pos, "u"),
+        move(pos, "r"),
+        move(pos, "d"),
+        move(pos, "l"),
+    ]
+
+
+def is_safe(board_state, pos):
+    print("safe?", pos)
+    if pos["x"] < 0:
+        return False
+    elif pos["x"] >= board_state["board"]["width"]:
+        return False
+    elif pos["y"] < 0:
+        print("Unsafe")
+        return False
+    elif pos["y"] >= board_state["board"]["height"]:
+        return False
+
+    for snake in board_state["board"]["snakes"]:
+        if pos in snake["body"][:-1]:
+            return False
+
+    return True
+
+
+def apply(board_state):
+    my_pos = board_state["you"]["body"][0]
+
+    return [
+        1 if is_safe(board_state, pos) else 0
+        for pos in surroundings(my_pos)
+    ]
